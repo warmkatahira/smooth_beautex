ef547a8ea31e7226a040998bf70b194d1875d3bc
warmkatahira <t.katahira@warm.co.jp>
2026-03-12 13:05:20 +0900

    20260312_1

--- resources/views/components/order/order-detail/order-info.blade.php
+++ resources/views/components/order/order-detail/order-info.blade.php
@@ -13,6 +13,7 @@
                 <x-order.order-detail.info-div label="注文日" :value="CarbonImmutable::parse($order->order_date)->isoFormat('Y年MM月DD日(ddd)')" />
                 <x-order.order-detail.info-div label="受注管理ID" :value="$order->order_control_id" />
                 <x-order.order-detail.info-div label="受注区分" :value="$order->order_category->order_category_name" />
+                <x-order.order-detail.info-div label="モール" :value="$order->order_category->mall->mall_name" />
             </div>
         </div>
     </div>
