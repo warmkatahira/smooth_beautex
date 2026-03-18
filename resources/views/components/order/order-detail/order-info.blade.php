88d4c0cc574de558ff7c066f9beab381058bf84c
warmkatahira <t.katahira@warm.co.jp>
2026-03-18 09:28:50 +0900

    20260318_1

--- resources/views/components/order/order-detail/order-info.blade.php
+++ resources/views/components/order/order-detail/order-info.blade.php
@@ -6,13 +6,14 @@
                 <x-order.order-detail.info-div label="注文ステータス" :value="OrderStatusEnum::getJpValueById($order->order_status_id)" />
                 <x-order.order-detail.info-div label="取込日時" :value="CarbonImmutable::parse($order->order_import_date . ' ' . $order->order_import_time)->isoFormat('Y年MM月DD日(ddd) HH:mm:ss')" />
                 <x-order.order-detail.info-div label="注文番号" :value="$order->order_no" />
+                <x-order.order-detail.info-div label="受注区分" :value="$order->order_category->order_category_name" />
             </div>
         </div>
         <div class="w-1/2">
             <div class="flex flex-col">
-                <x-order.order-detail.info-div label="注文日" :value="CarbonImmutable::parse($order->order_date)->isoFormat('Y年MM月DD日(ddd)')" />
+                <div class="h-[24px]"></div>
+                <x-order.order-detail.info-div label="注文日時" :value="CarbonImmutable::parse($order->order_date . ' ' . $order->order_time)->isoFormat('Y年MM月DD日(ddd) HH:mm:ss')" />
                 <x-order.order-detail.info-div label="受注管理ID" :value="$order->order_control_id" />
-                <x-order.order-detail.info-div label="受注区分" :value="$order->order_category->order_category_name" />
                 <x-order.order-detail.info-div label="モール" :value="$order->order_category->mall->mall_name" />
             </div>
         </div>
