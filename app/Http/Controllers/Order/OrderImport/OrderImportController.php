1c10a229246c3e28478eece5fbfdb6b9b78c7e5e
warmkatahira <t.katahira@warm.co.jp>
2026-03-24 15:39:56 +0900

    20260324_1

--- app/Http/Controllers/Order/OrderImport/OrderImportController.php
+++ app/Http/Controllers/Order/OrderImport/OrderImportController.php
@@ -103,7 +103,7 @@ class OrderImportController extends Controller
                 // 受注管理IDを採番
                 $OrderImportService->updateOrderControlId();
                 // Qoo10の場合
-                if($order_category_id === MallEnum::QOO10_ID){
+                if($mall_id === MallEnum::QOO10_ID){
                     // 「配送会社」を注文番号毎で1つになるように更新
                     $OrderImportForQoo10Service->updateMallShippingMethod();
                 }
