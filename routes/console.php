e572d0252779b1189eb1b0c17c57636c65a84a84
warmkatahira <t.katahira@warm.co.jp>
2026-04-08 14:40:12 +0900

    20260408_1

--- routes/console.php
+++ routes/console.php
@@ -16,9 +16,11 @@ Schedule::command('item_upload_file_delete')->dailyAt('02:00');
 // 商品アップロードエラーの削除を毎日「02:00」に実行
 Schedule::command('item_upload_error_file_delete')->dailyAt('02:00');
 // 商品ロケーション更新の削除を毎日「02:00」に実行
-Schedule::command('location_update_file_delete')->everyMinute();
+Schedule::command('location_update_file_delete')->dailyAt('02:00');
 // 荷札データ発行ディレクトリと履歴レコードの削除を毎日「02:00」に実行
-Schedule::command('nifuda_issue_delete')->dailyAt('02:00');
+Schedule::command('nifuda_issue_delete')->everyMinute();
+// 出荷完了エラーの削除を毎日「02:00」に実行
+Schedule::command('shipping_work_end_error_file_delete')->dailyAt('02:00');
 // DBバックアップの削除を毎日「03:00」に実行
 Schedule::command('backup_db_delete')->dailyAt('03:00');
 // DBバックアップを毎日「03:30」に実行
